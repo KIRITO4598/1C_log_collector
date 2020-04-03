6b205b1caef942d93478312847275e26e4ccb0bf
Kirill <kirill.kirill4598@mail.ru>
2020-04-03 19:17:51 +0300

    Initial commit

---
+++ 1C_log_collector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1C_log_collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
